602363adf2f41fe93881df92c4d2a1521e874a7f
Dmitriy Petrukhin <petrukhindmitriy@gmail.com>
2020-07-10 11:18:31 +0300

    init

---
+++ src/components/Game/Game.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './Game.module.scss'
+import PropTypes from 'prop-types';
+
+const Game = (props) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+Game.propTypes = {
+
+};
+
+export default Game;
